ad1782551c45e30bb2128a0bd432e2f8140a1615
mykola <mykola.khlopyk@gmail.com>
2023-07-07 02:59:12 +0300

    created response

---
+++ src/main/java/com/scisw/phonecontacts/domain/login/response/JwtAuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.scisw.phonecontacts.domain.login.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class JwtAuthenticationResponse {
+    private String token;
+}
