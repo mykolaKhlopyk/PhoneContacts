d29023adc64bca67da3431be91ff163df03e09da
mykola <mykola.khlopyk@gmail.com>
2023-07-07 02:58:37 +0300

    created SignUpRequest and SignInRequest

---
+++ src/main/java/com/scisw/phonecontacts/domain/login/request/SignInRequest.java
@@ -0,0 +1,14 @@
+package com.scisw.phonecontacts.domain.login.request;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignInRequest {
+    private String login;
+    private String password;
+}
---
+++ src/main/java/com/scisw/phonecontacts/domain/login/request/SignUpRequest.java
@@ -0,0 +1,15 @@
+package com.scisw.phonecontacts.domain.login.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignUpRequest {
+    private String login;
+    private String password;
+}
